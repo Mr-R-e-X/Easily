b4a596dfcf6f0cfa168360e45ca0781b0ffc92e5
MrRex <souvikhazra151@gmail.com>
2024-08-19 18:32:49 +0530

    port changed

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import app from "./index.js";
 import connectToDB from "./config/mongodb.js";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 connectToDB()
   .then(() => {
