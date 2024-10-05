06d6fb13ca589df09ae4d35391f1fa2139843a70
MrRex <souvikhazra151@gmail.com>
2024-10-05 11:57:06 +0530

    swagger docs added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import "./env.js";
+
 import express from "express";
 import cookieParser from "cookie-parser";
 import path from "path";
 import cors from "cors";
 import connectToDB from "./config/mongodb.js";
 const app = express();
+
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+
+// Load the YAML file
+const swaggerDocument = YAML.load(path.resolve("src", "swagger.yaml"));
 
 app.use(
   cors({
@@ -16,6 +23,8 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static(path.resolve("public")));
 app.use(cookieParser());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (req, res) => {
   res.send("Welcome to Easily the Job Board API!");
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -60,3 +60,4 @@ export const verifyAdminJWT = asyncHandler(async (req, res, next) => {
     throw new ApiError(401, error.message || "Invalid Access Token");
   }
 });
+
